a27c92f36e5d9cacd70df66e6bdaf3d2b0b4d2cb
Kotoshka2 <myrzabekov.madiyar@mail.ru>
2024-10-04 22:11:32 +0500

    added pathfinding, started ai development

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public enum EnemyType
+    {
+        Range,
+        Melee
+    }
+    private Camera mainCam;
+    private NavMeshAgent agent;
+    private Vector3 target;
+    private GameObject player;
+    [SerializeField]
+    private EnemyType type;
+    void Start()
+    {
+        mainCam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    
+    void Update()
+    {
+        switch (type)
+        {
+            case EnemyType.Melee:
+                target = new Vector3(player.transform.position.x,1,player.transform.position.z);
+                
+                break;
+            case EnemyType.Range:
+                target = Vector3.Lerp(this.transform.position, player.transform.position,0.5f);
+                
+                break;
+        }
+        agent.SetDestination(target);
+    }
+}
